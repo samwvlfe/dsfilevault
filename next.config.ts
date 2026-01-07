56368bc89d36dff114804e597e5419c64c936b82
samwvlfe <r.sam.wolfe@gmail.com>
2026-01-07 13:06:41 -0500

    changes to work flow and next.config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: "export",
 };
-module.exports = nextConfig;
+
+export default nextConfig;
