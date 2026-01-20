6286aa504b6232416579ee7c4c72cd51cb99f824
samwvlfe <r.sam.wolfe@gmail.com>
2026-01-20 12:02:36 -0500

    add mainshell css back in

--- components/mainshell.tsx
+++ components/mainshell.tsx
@@ -2,11 +2,18 @@
 
 // import { useEffect, useState } from "react";
 // import Image from "next/image";
-// import styles from "./mainshell.module.css";
+import styles from "./mainshell.module.css";
 import { FileContent } from "@/components/filecontent";
 
 export function MainShell() {
     return (
-        <FileContent />
+        <div className={styles.wrapper}>
+            {/* <div className={`${styles.infobar} row center`}>
+                <div className={styles.txt}>
+                    All Files Accessable by DockStar employees on OneDrive - here in one place.
+                </div>
+            </div> */}
+            <FileContent />
+        </div>
     );
 }
